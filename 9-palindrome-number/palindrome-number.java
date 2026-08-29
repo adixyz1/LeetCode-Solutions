4cf03dbd0c96991e0d8aa992c914283178d1617c
Aditya Gupta <adityagupta80098@gmail.com>
2026-08-29 17:57:29 +0530

    Time: 5 ms (82.10%) | Memory: 45.7 MB (94.04%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp = x;
+        int reverse = 0;
+        while (temp >0){
+            int digit = temp  % 10;
+            reverse = reverse * 10 + digit;
+            temp = temp /10;
+        }
+        if(x == reverse){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
