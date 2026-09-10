7d62fa3c53bee780bd9c8aac75eefcd308b9217d
Aditya Gupta <adityagupta80098@gmail.com>
2026-09-10 12:47:50 +0530

    Time: 1 ms (85.90%) | Memory: 42.5 MB (57.33%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -3,8 +3,8 @@ class Solution {
         // return Math.max(0,n-999);
         if(n<999){
             return 0;
-        } else{
-            return n-999;
         }
+        return n-999;
+        
     }
 }
