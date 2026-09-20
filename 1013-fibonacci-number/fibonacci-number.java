2769ca21cf5dc0748c8d0efc52d4cf52070a3143
Aditya Gupta <adityagupta80098@gmail.com>
2026-09-20 18:42:26 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (70.33%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -3,6 +3,16 @@ class Solution {
         if (n == 0 || n == 1){
             return n;
         }
-        return fib(n-1) + fib(n-2);
+        int firstTerm = 0;
+        int secondTerm = 1;
+
+        for(int i = 0 ; i<n ; i++){
+            int thirdTerm = firstTerm + secondTerm ;
+
+            firstTerm = secondTerm;
+            secondTerm = thirdTerm;
+        }
+        return firstTerm;
+        // return fib(n-1) + fib(n-2);
     }
 }
